dfb075e840f0bcc914869a6dd4e357e39cf9d52d
sunnygoyal888 <goyalsunny888@gmail.com>
2023-07-08 22:53:28 +0530

    Initial commit

---
+++ src/main/java/com/project/QuizApp/service/QuestionService.java
@@ -0,0 +1,45 @@
+package com.project.QuizApp.service;
+
+import com.project.QuizApp.dao.QuestionDao;
+import com.project.QuizApp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionDao questionDao;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new String(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/project/QuizApp/service/QuizService.java
@@ -0,0 +1,63 @@
+package com.project.QuizApp.service;
+
+import com.project.QuizApp.dao.QuestionDao;
+import com.project.QuizApp.dao.QuizDao;
+import com.project.QuizApp.model.Question;
+import com.project.QuizApp.model.QuestionWrapper;
+import com.project.QuizApp.model.Quiz;
+import com.project.QuizApp.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    private QuizDao quizDao;
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, Integer numQ, String title) {
+        List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for(Question q: questionFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for(Response response: responses){
+            if(response.getResponse().equals(questions.get(i).getRightAnswer())){
+                right++;
+            }
+            i++;
+        }
+
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
